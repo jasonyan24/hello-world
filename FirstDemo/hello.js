5275fdbfad66b87488e1ec089970d89f1da9b840
jasonyan24 <xingbo.yan24@gmail.com>
2018-09-09 14:15:07 -0300

    git repo test

---
+++ FirstDemo/hello.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/*
+ This is a test
+ */
+
+function main()
+{
+    var name ='jason';
+
+    console.log("Hello, " + name + "!");
+}
+
+if (require.main == module)
+{
+    main();
+}
